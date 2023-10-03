8d7baeab26d59de20a3fc35469b8130a404bcef2
mlohithraj <146320968+mlohithraj@users.noreply.github.com>
2023-10-03 22:42:45 +0530

    Add files via upload

--- demo.js
+++ demo.js
@@ -48,14 +48,41 @@
 // let lastItem= document.querySelector('.list-group-item:last-child');
 // lastItem.style.backgroundColor= 'yellow';
 
-let secondItem= document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color= 'green';
+// let secondItem= document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color= 'green';
 
-let odd= document.querySelectorAll('li:nth-child(odd)');
-let even= document.querySelectorAll('li:nth-child(even)');
+// let odd= document.querySelectorAll('li:nth-child(odd)');
+// let even= document.querySelectorAll('li:nth-child(even)');
 
-for(let i=0; i<odd.length; i++){
-    odd[i].style.backgroundColor= 'green';
-    even[i].style.backgroundColor= '#ccc';
+// for(let i=0; i<odd.length; i++){
+//     odd[i].style.backgroundColor= 'green';
+//     even[i].style.backgroundColor= '#cccc';
+// }
 
-}
+let itemList=  document.querySelector('#items');
+console.log(itemList.parentNode);
+itemList.parentNode.style.color= 'red';
+
+//parentElement
+
+
+var myDiv= document.createElement('div');
+
+myDiv.className= 'Hello';
+
+myDiv.setAttribute('title', 'Hello');
+
+var nyDivTxt= document.createTextNode('Hello');
+
+myDiv.appendChild(nyDivTxt);
+
+// let container= document.querySelector('header .container');
+// let h1= document.querySelector('header h1')
+
+// console.log(myDiv);
+// container.insertBefore(myDiv, h1);
+
+let listGroup= document.querySelector('body .title');
+let h2= document.querySelector('body h2');
+
+listGroup.insertBefore(myDiv, h2);
