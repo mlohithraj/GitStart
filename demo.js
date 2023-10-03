5c20037368faea6360105e6fe4f8bb3734a018be
mlohithraj <146320968+mlohithraj@users.noreply.github.com>
2023-10-03 20:14:22 +0530

    Add files via upload

--- demo.js
+++ demo.js
@@ -19,3 +19,7 @@ console.log(items);
 console.log(items[1]);
 items[1].style.fontWeight= 'bold';
 items[1].style.backgroundColor= 'Green';
+
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight="bold";
+}
