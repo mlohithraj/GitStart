87aa0a99e854a75e2c483aae6f9cf09d707ee0f7
mlohithraj <146320968+mlohithraj@users.noreply.github.com>
2023-10-03 20:58:40 +0530

    Add files via upload

--- demo.js
+++ demo.js
@@ -1,18 +1,18 @@
 //console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.head);
+// console.log(document.body);
 
-console.log(document.getElementById('header-title'));
-let headerTitle= document.getElementById('header-title');
-let header= document.getElementById('main-header');
+// console.log(document.getElementById('header-title'));
+// let headerTitle= document.getElementById('header-title');
+// let header= document.getElementById('main-header');
 //console.log(headerTitle);
 //headerTitle.textContent= 'Hello';
 //headerTitle.innerText= 'Goodbye';
 //headerTitle.innerHTML= '<h3>Hello<h3>'
 
-header.style.borderBottom= 'solid 5px #000';
+// header.style.borderBottom= 'solid 5px #000';
 
 // let items= document.getElementsByClassName('list-group-item');
 // console.log(items);
@@ -26,12 +26,36 @@ header.style.borderBottom= 'solid 5px #000';
 
 
 
-let li= document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].style.fontWeight= 'bold';
-li[1].style.backgroundColor= 'Green';
+// let li= document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].style.fontWeight= 'bold';
+// li[1].style.backgroundColor= 'Green';
 
-for(let i=0; i<li.length; i++){
-    li[i].style.fontWeight="bold";
+// for(let i=0; i<li.length; i++){
+//     li[i].style.fontWeight="bold";
+// }
+
+// let header= document.querySelector('#main-header');
+// header.style.borderBottom= 'solid 3px yellow';
+
+// let input= document.querySelector('input');
+// input.value= 'Hi';
+
+// let items= document.querySelector('.list-group-item');
+// items.style.color= 'blue';
+
+// let lastItem= document.querySelector('.list-group-item:last-child');
+// lastItem.style.backgroundColor= 'yellow';
+
+let secondItem= document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color= 'green';
+
+let odd= document.querySelectorAll('li:nth-child(odd)');
+let even= document.querySelectorAll('li:nth-child(even)');
+
+for(let i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor= 'green';
+    even[i].style.backgroundColor= '#ccc';
+
 }
